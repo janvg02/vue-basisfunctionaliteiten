f714ee40a87b3c20836b3c9ef25fe5cdf3141489
janvg02 <jan.vangalen@student.pxl.be>
2023-10-09 12:26:19 +0200

    inleiding gedaan

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <script>
-
+export default{
+  data(){
+    return{
+      mainTitle: "Welkom in de Vue App"
+    }
+  }
+}
 </script>
 
 <template>
-  <h1>Welkom in de Vue App</h1>
+  <h1>{{ mainTitle }}</h1>
+  <label v-bind:for="firstnameFormElement">{{firstnameFormElement}}</label>
+  <input type="text": name="firstnameFormElement" v-bind:id="firstnameFormElement">
 </template>
 
 <style>
