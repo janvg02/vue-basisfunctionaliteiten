88fc240d99d43ae884c7aa10d6a8747b11d7e1e7
janvg02 <jan.vangalen@student.pxl.be>
2023-10-16 11:08:27 +0200

    opslaan

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,22 @@
 export default{
   data(){
     return{
-      mainTitle: "Welkom in de Vue App"
+      naam: "JAN"
+    }
+  },
+  methods: {
+    showText(){
+      return this.naam;
     }
   }
 }
 </script>
 
 <template>
-  <h1>{{ mainTitle }}</h1>
-  <label v-bind:for="firstnameFormElement">{{firstnameFormElement}}</label>
-  <input type="text": name="firstnameFormElement" v-bind:id="firstnameFormElement">
+  <h1>Welkom in het project</h1>
+  <p>{{showText() }}</p>
 </template>
+
 
 <style>
 </style>
